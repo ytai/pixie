22d7516494642bf88fa201db5f4b60214ca48f23
Ytai Ben-Tsvi <ytaibt@gmail.com>
2014-06-14 22:46:56 -0700

    Sorta workin'

--- software/main.c
+++ software/main.c
@@ -3,7 +3,7 @@
 
 #include <xc.h>
 
-#pragma config FOSC = INTOSC, WDTE = OFF, CP = OFF, PLLEN = ON
+#pragma config FOSC = INTOSC, WDTE = ON, CP = OFF, PLLEN = ON, BOREN = OFF, MCLRE = ON, CLKOUTEN = OFF, PWRTE = OFF, LPBOREN = ON,
 
 #define DIN_PIN_MASK (1 << 0)
 #define DOUT_PIN_MASK (1 << 1)
@@ -11,6 +11,26 @@
 #define GREEN_PIN_MASK (1 << 4)
 #define BLUE_PIN_MASK  (1 << 5)
 #define RGB_PIN_MASK (RED_PIN_MASK | GREEN_PIN_MASK | BLUE_PIN_MASK)
+
+// How long of a silence on the line is considered a latch event.
+// Units are 32[us] per count.
+#define LATCH_TIME 30
+
+#define TEMPERATURE_20C 82
+#define TEMPERATURE_30C 76
+#define TEMPERATURE_40C 69
+#define TEMPERATURE_50C 62
+#define TEMPERATURE_60C 56
+#define TEMPERATURE_70C 49
+
+// To convert this value to real temperature (in Celcius):
+ // T = 144.43 - temperature * 1.5151515
+static uint8_t temperature;
+
+static uint16_t t;
+static uint8_t r, g, b;
+uint8_t color_array[24];
+volatile uint8_t bit_count;
 
 static void InitializePwm() {
   // Fosc, no prescaler = 32MHz. Full cycle will be: 32MHz / 2^16 = 488Hz
@@ -34,9 +54,9 @@ static void InitializePwm() {
   PWM3OFH = PWM3OFL = 0;
 
   // Set the duty cycle to 0.
-  PWM1DCH = PWM1DCL = 0;
-  PWM2DCH = PWM2DCL = 0;
-  PWM3DCH = PWM3DCL = 0;
+  PWM1DC = 0;
+  PWM2DC = 0;
+  PWM3DC = 0;
 
   // Latch the values on next trigger.
   PWMLD = 0x07;
@@ -63,15 +83,69 @@ static void InitializePins() {
   TRISA = ~(RGB_PIN_MASK | DOUT_PIN_MASK);
 }
 
-static void InitializeTimer() {
+static void InitializeTimers() {
+  // Timer 0 : Instruction clock / 256 = 31.25kHz (32us).
   OPTION_REG = 0x07;
+
+  // Timer 2: Instruction clock / 16 = 500kHz (2us).
+  T2CON = 0x06;
 }
 
-uint8_t color_array[24];
-volatile uint8_t bit_count;
+static void InitializeTemperature() {
+  // Enable temperature indicator at high range and FVR.
+  FVRCON = 0xB0;
 
-void LatchColor() {
-    uint8_t r, g, b, i;
+  // Configure ADC.
+  ADCON2 = 0x00;
+  // Left-aligned, Tad = 1us.
+  ADCON1 = 0x20;
+
+  // Wait for FVR to settle.
+  while (!FVRRDY);
+}
+
+static inline void Timer2Delay(uint8_t count) {
+  TMR2 = 0;
+  while (TMR2 < count);
+}
+
+static void ReadTemperature() {
+  // Configure the ADC to acquire the temperature indicator.
+  ADCON0 = 0x75;
+
+  // Wait for acquisition (200us).
+  Timer2Delay(200);
+
+  // Sample.
+  GO = 1;       // Start.
+  while (GO);   // Wait completion.
+
+  t = ADRES;
+
+    // Enable the FVR amplifier (2x)
+  FVRCON = 0xB2;
+
+  // Configure the ADC to acquire the FVR.
+  ADCON0 = 0x7D;
+
+  // Wait for acquisition (10us).
+  Timer2Delay(10);
+
+  // Sample.
+  GO = 1;       // Start.
+  while (GO);   // Wait completion.
+
+  // Disable the FVR amplifier.
+  FVRCON = 0xB0;
+
+  temperature = (0xFFC0 - t) / ADRESH;
+}
+
+static void ConvertColor() {
+  // Note the the temperature units used are reversed: bigger number == lower
+  // temperature. So the condition below actually means temp <= 60C.
+  if (temperature >= TEMPERATURE_60C && bit_count == 1) {
+    uint8_t i;
     uint8_t const * p = color_array;
 
     // Read Green bits.
@@ -94,32 +168,30 @@ void LatchColor() {
       b <<= 1;
       b |= (*p++ & 1);
     }
+  }
+}
 
+static void LatchColor() {
     // Prepare the new values in the PWM periods.
-    PWM3DCH = r;
-    PWM3DCL = r;
-    PWM2DCH = g;
-    PWM2DCL = g;
-    PWM1DCH = b;
-    PWM1DCL = b;
+    PWM3DC = (uint16_t) r * r;
+    PWM2DC = (uint16_t) g * g;
+    PWM1DC = (uint16_t) b * b;
 
     // Latch all at once.
     PWMLD = 0x07;
+
+    // Force off until told otherwise.
+    r = g = b = 0;
 }
 
-// TODO:
-// - Watchdog.
-// - Over-temperature protection.
-
 void main() {
-  uint16_t i = 0;
-  uint16_t delay;
   // High speed.
   OSCCONbits.IRCF = 0xe;
 
   InitializePwm();
   InitializePins();
-  InitializeTimer();
+  InitializeTimers();
+  InitializeTemperature();
 
   // Enable interrupts on rising edge of DIN.
   IOCAP0 = 1;
@@ -127,13 +199,14 @@ void main() {
 
   // Wait for silence. Do not take interrupts and do not set the output.
   TMR0 = 0;
-  while (TMR0 < 30) {
+  while (TMR0 < LATCH_TIME) {
     if (IOCAF0) {
       TMR0 = 0;
       IOCAF0 = 0;
     }
   }
 
+  // Main loop.
   while (true) {
     // FSR0 needs to point to the beginning of the color array.
     FSR0 = (uint16_t) color_array;
@@ -147,12 +220,31 @@ void main() {
     // Wait for read to begin, followed by a silence.
     // In the meantime, the interrupt handler does the job.
     while (bit_count == 25);
+
     TMR0 = 0;
-    while (TMR0 < 30);
+
+    // While we're waiting, read the temperature. This should take ~250us, so
+    // we won't be missing our latch time, which is much greater.
+    ReadTemperature();
+
+    // And also while we're waiting, if we're done reading our own value,
+    // convert it. If we got a silence before having read 24 bits, we will
+    // simply skip the conversion and latch the previous values.
+    while (TMR0 < LATCH_TIME) {
+      if (bit_count == 1) {
+        ConvertColor();
+        break;
+      }
+    }
+
+    while (TMR0 < LATCH_TIME);
 
     // Disable interrupts while processing.
     GIE = 0;
 
+    // Clear the watchdog.
+    CLRWDT();
+  
     // The color array not contains the 24 bits we've read. Latch.
     LatchColor();
   }
--- software/read.asm
+++ software/read.asm
@@ -2,8 +2,9 @@
 
 GLOBAL _bit_count
 
-; This entire handler takes 15 cycles to execute.
+; This entire handler takes 16 cycles to execute.
 PSECT intentry,class=CODE,reloc=2,delta=2
+    BANKSEL PORTA
     movf PORTA, w         ; Sample PORTA. Bit 0 is the input.
     decfsz _bit_count, f  ; Decrement bit count. If zero, we're in echo mode.
     bra read_mode
@@ -11,15 +12,15 @@ PSECT intentry,class=CODE,reloc=2,delta=2
     ; echo mode handling.
     bsf PORTA, 1          ; Generate rising edge (bit 1).
     lslf WREG, w          ; Shift the input bit to the output position.
+    nop
     movwf PORTA           ; We're setting all PORTA bits, but bit 1 is the only output.
     movlw 1               ; Increment bit count.
     addwf _bit_count
     clrf PORTA            ; Generate falling edge.
-    nop
 
   cleanup:
     clrf TMR0             ; Clear the idle time counter.
-    BANKSEL(IOCAF)        ; Clear the interrupt.
+    BANKSEL IOCAF         ; Clear the interrupt.
     clrf BANKMASK(IOCAF)
     retfie
 
@@ -29,7 +30,7 @@ PSECT intentry,class=CODE,reloc=2,delta=2
 
     ; Save FSR0 to the shadow register.
     movf FSR0L, w
-    BANKSEL(FSR0L_SHAD)
+    BANKSEL FSR0L_SHAD
     movwf BANKMASK(FSR0L_SHAD)
 
     bra cleanup
